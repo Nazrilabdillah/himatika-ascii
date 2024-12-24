e17645864aa92ef17e534f3dcb403742c3c8aa1b
Nazrilabdillah <131126678+Nazrilabdillah@users.noreply.github.com>
2024-12-24 17:52:29 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask,Response
+import os
+import time
+import random
+from PIL import Image as img
 
+
+
+def fr(path):
+    file = open("bird_text/"+path,"r")
+    return file.read()
 app = Flask(__name__)
-
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def animated_ascii():
+    def generate():
 
-@app.route('/about')
-def about():
-    return 'About'
+         while True:
+            frame = open("logo_ascii.txt","r").read()
+            ran_num = random.randint(33,40)
+            yield f"\033[1;34m{frame}\n"
+            time.sleep(5)
+            
+    return Response(generate(), mimetype='text/plain')
+@app.route('/bird')
+def index():
+    def generate():
+        while True:
+            for frame in os.listdir("bird_text"):
+                 ran_num = random.randint(31,40)
+                 yield f"\033[1;{ran_num}m{fr(frame)}\n"
+                 time.sleep(0.2)  # jeda antar frame
+    return Response(generate(), mimetype='text/plain')
+
+
+
